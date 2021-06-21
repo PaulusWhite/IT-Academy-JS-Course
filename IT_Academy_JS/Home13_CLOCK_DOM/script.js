3865570eb5d4f34f1a67e188ea8be095850b2889
Pavel <pauledelweissontop@gmail.com>
2021-06-21 20:18:45 +0300

    add clock homework

--- IT_Academy_JS/Home13_CLOCK_DOM/script.js
+++ IT_Academy_JS/Home13_CLOCK_DOM/script.js
@@ -1,14 +1,35 @@
-let mainClock = document.querySelector(".main_clock");
-
+let mainClock = document.querySelector(".main_clock"); //Гланый блок для часов
+let arrowForSeconds = document.querySelector(".boxForSeconds"); //Видимая стрелка для секунд
+let arrowForMinutes = document.querySelector(".boxForMinutes"); //Видимая стрелка для минут
+let arrowForHours = document.querySelector(".boxForHours"); //Видимая стрелка для часов
 function createClock(){
-    let top = 60;
-    let left = 304;
-    for(let i = 1;i <=2;i++){
-        let numClock = document.createElement("div");
-        numClock.style.cssText = "width:60px;height:60px;border-radius:100%;background-color:green;position:absolute;transform:translate(-50%,-50%)";
-        numClock.style.top = top + "px";
-        numClock.style.left = left + "px";
-        mainClock.append(numClock);
+    let rotateForNumClock = 0; //Счётчик свойства rotate для основного блока элемента циферблата
+    let rotateForDivForNumber = 0; //Счётчик свойства rotate непосредственно для блока числа
+    for(let i = 0;i <12;i++){
+        let numClock = document.createElement("div"); //Создал основной блок элемента циферблата
+        let divForNumber = document.createElement("div"); //Создал блок непосредственно для числа
+        divForNumber.style.cssText = `;transform:translate(-50%,0)rotate(${rotateForDivForNumber}deg);`;
+        divForNumber.textContent = i;
+        numClock.style.cssText = `transform:rotate(${rotateForNumClock}deg);`;
+        mainClock.appendChild(numClock).append(divForNumber);
+        rotateForNumClock+=30;
+        rotateForDivForNumber-=30;
     }
 }
-createClock()
+createClock();
+let countClockForSec = -90;
+let countClockForMinutes = -90;
+let countClockForHours = -90;
+setInterval(function(){
+    arrowForSeconds.style.cssText = `transform: translate(-50%, -50%) rotate(${countClockForSec+6}deg);`;
+    countClockForSec+=6;
+},1000);
+setInterval(function(){
+    arrowForMinutes.style.cssText = `transform: translate(-50%, -50%) rotate(${countClockForMinutes+6}deg);`;
+    countClockForMinutes+=6;
+},1000*60);
+setInterval(function(){
+    arrowForHours.style.cssText = `transform: translate(-50%, -50%) rotate(${countClockForHours+1}deg);`;
+    countClockForHours+=6;
+},1000*3600)
+
--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-let elem = document.querySelector(".clock");
+let elem = document.querySelector(".first");
+let elemSecond = document.querySelector(".second");
+let elemThird = document.querySelector(".third");
+console.log(elem)
+
+elem.addEventListener("click",function(){
+    alert("First")
+})
