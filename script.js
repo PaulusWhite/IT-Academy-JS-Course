3e3aa9f68eb9dd99f655901cea753a0a5536a9a8
PaulusWhite <72739376+PaulusWhite@users.noreply.github.com>
2021-06-21 20:25:46 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let elem = document.querySelector(".first");
-let elemSecond = document.querySelector(".second");
-let elemThird = document.querySelector(".third");
-console.log(elem)
-
-elem.addEventListener("click",function(){
-    alert("First")
-})
