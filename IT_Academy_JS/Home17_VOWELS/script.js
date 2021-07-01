d100e78b6b43414cc0fe775ed2cb9624b81a7023
Pavel <pauledelweissontop@gmail.com>
2021-07-01 14:27:40 +0300

    homework17

--- IT_Academy_JS/Home17_VOWELS/script.js
+++ IT_Academy_JS/Home17_VOWELS/script.js
@@ -1,15 +1,15 @@
 const vowels = (str) => {
 str = str.split("");
 let lowRuVowels = ["ё","у","е","ы","а","о","э","я","и","ю"];
-let upRuVowels = lowRuVowels.map(function(elem){
+let upRuVowels = lowRuVowels.map((elem) => {
     return elem.toUpperCase();
 });
 let allRuVowels = [lowRuVowels,upRuVowels].reduce((firstArr,secondArr) => {
     return firstArr.concat(secondArr);
 });
 let current = 0;
-    str.forEach(function(elem){
-        allRuVowels.filter(function(word){
+    str.forEach((elem) => {
+        allRuVowels.filter((word) => {
             if(word === elem){
                 current++;
             }
