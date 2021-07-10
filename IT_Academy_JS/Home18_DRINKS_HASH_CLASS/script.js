8b83013b225b3a28f50ba2395f4e5b5b9037d0ca
Pavel <pauledelweissontop@gmail.com>
2021-07-10 20:10:40 +0300

    18 homework

---
+++ IT_Academy_JS/Home18_DRINKS_HASH_CLASS/script.js
@@ -0,0 +1,65 @@
+class HashStorageClass {
+    constructor(){
+        this.obj = {
+        }
+        this.addValue = (key,value) => {
+            this.obj[key] = value;
+        }
+        this.getValue = (key) => {
+            if(this.obj[key]){
+                return this.obj[key];
+            }else{
+                return "Такого напитка в хранилище нет!";
+            }
+        }
+        this.deleteValue = (key) => {
+            if(this.obj[key]){
+                delete this.obj[key];
+                return true;
+            }else if(!this.obj[key]){
+                return false;
+            }
+        }
+        this.getKeys = () => {
+            this.arr = [];
+            for(let key in this.obj){
+                this.arr.push(key);
+            }
+            return this.arr;
+        }
+    }
+}
+let drinkStorage = new HashStorageClass();
+let getKey = () => {
+    let key = prompt("Укажите название напитка");
+    return key;
+}
+let getValue = () => {
+    let isAlco = confirm("Ваш напиток содержит алкоголь?");
+    if(isAlco === true){
+        isAlco = "Напиток содержит алкоголь";
+    }else{
+        isAlco = "Напиток безалкогольный";
+    }
+    let recipe = prompt("Укажите рецептуру приготовления вашего напитка");
+    let value = "Алкоголь: " + isAlco + " \nРецептура приготовления: " + recipe;
+    return value;
+}
+
+let addVl = document.getElementById("addValue");
+let getVl = document.getElementById("getValue");
+let deleteVl = document.getElementById("deleteValue");
+let allVl = document.getElementById("getKeys");
+
+addVl.addEventListener("click",() => {
+    drinkStorage.addValue(getKey(),getValue());
+})
+getVl.addEventListener("click",() => {
+    alert(drinkStorage.getValue(getKey()));
+})
+deleteVl.addEventListener("click",() => {
+    alert(drinkStorage.deleteValue(getKey()));
+})
+allVl.addEventListener("click",() => {
+    alert(drinkStorage.getKeys());
+})
