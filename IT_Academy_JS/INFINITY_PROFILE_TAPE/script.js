853aef8fe34c281a81772d37c8d4c12c3c9a61eb
Pavel <pauledelweissontop@gmail.com>
2021-07-23 15:30:36 +0300

    Infinity render users

--- IT_Academy_JS/INFINITY_PROFILE_TAPE/script.js
+++ IT_Academy_JS/INFINITY_PROFILE_TAPE/script.js
@@ -1,5 +1,6 @@
-let renderUsers = () => {
-    fetch("https://randomuser.me/api/")
+window.addEventListener("load",() => {
+    for(let i = 0;i <12; i++){
+        fetch("https://randomuser.me/api/")
     .then(
         (res) => {
             return res.json();
@@ -21,11 +22,6 @@ let renderUsers = () => {
     </div>
         `
     })
-}
-
-window.addEventListener("load",() => {
-    for(let i = 0;i <12; i++){
-        renderUsers()
     }
 })
 
@@ -34,6 +30,27 @@ window.addEventListener("scroll",() => {
     let allposts = document.querySelectorAll(".main-box");
     let elem = allposts[allposts.length-1].getBoundingClientRect().bottom
     if(currentScroll > (elem + pageYOffset - 1200)){
-        renderUsers()
+        fetch("https://randomuser.me/api/")
+    .then(
+        (res) => {
+            return res.json();
+        }
+    )
+    .then((data) => {
+        let fullName = data.results[0].name
+        let img = data.results[0].picture.large;
+        let body = document.body;
+        body.innerHTML += `
+        <div class="main-box">
+        <div class="box">
+            <div class="box-for-img">
+                <img src="${img}">
+            </div>
+            <p>My name is</p>
+            <p class="name">${fullName.title} ${fullName.first} ${fullName.last}</p>
+        </div>
+    </div>
+        `
+    })
     }
 })
