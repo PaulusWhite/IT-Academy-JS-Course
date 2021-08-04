c1a154b3caf6784f9eec3001d6bd753365fe1c95
Pavel <pauledelweissontop@gmail.com>
2021-08-04 15:58:38 +0300

    infinity users

--- IT_Academy_JS/Home21_INFINITY_PROFILE_TAPE/script.js
+++ IT_Academy_JS/Home21_INFINITY_PROFILE_TAPE/script.js
@@ -1,16 +1,11 @@
-window.addEventListener("load",() => {
-    for(let i = 0;i <12; i++){
-        fetch("https://randomuser.me/api/")
-    .then(
-        (res) => {
-            return res.json();
-        }
-    )
-    .then((data) => {
-        let fullName = data.results[0].name
-        let img = data.results[0].picture.large;
-        let body = document.body;
-        body.innerHTML += `
+let renderUsers = async () => {
+    for(let i = 0;i < 12; i++){
+    let res = await fetch("https://randomuser.me/api/");
+    let data = await res.json();
+    let fullName = data.results[0].name
+    let img = data.results[0].picture.large;
+    let body = document.body;
+    body.innerHTML += `
         <div class="main-box">
         <div class="box">
             <div class="box-for-img">
@@ -19,38 +14,16 @@ window.addEventListener("load",() => {
             <p>My name is</p>
             <p class="name">${fullName.title} ${fullName.first} ${fullName.last}</p>
         </div>
-    </div>
-        `
-    })
+        </div>
+         `
     }
-})
+}
+renderUsers();
 
 window.addEventListener("scroll",() => {
-    let currentScroll = window.pageYOffset ;
-    let allposts = document.querySelectorAll(".main-box");
-    let elem = allposts[allposts.length-1].getBoundingClientRect().bottom
-    if(currentScroll > (elem + pageYOffset - 1200)){
-        fetch("https://randomuser.me/api/")
-    .then(
-        (res) => {
-            return res.json();
-        }
-    )
-    .then((data) => {
-        let fullName = data.results[0].name
-        let img = data.results[0].picture.large;
-        let body = document.body;
-        body.innerHTML += `
-        <div class="main-box">
-        <div class="box">
-            <div class="box-for-img">
-                <img src="${img}">
-            </div>
-            <p>My name is</p>
-            <p class="name">${fullName.title} ${fullName.first} ${fullName.last}</p>
-        </div>
-    </div>
-        `
-    })
+    let elemsNum = document.body.children.length-2;
+    let current = document.documentElement.scrollHeight - 937;
+    if(current === window.pageYOffset && (elemsNum % 12 === 0)){
+        renderUsers();
     }
 })
