1fea9a6c6b10b419d8ccedc1ce151b9309c0a0ba
PaulusWhite <72739376+PaulusWhite@users.noreply.github.com>
2021-12-24 22:55:44 +0300

    Delete IT_Academy_JS/MyProject directory

--- IT_Academy_JS/MyProject/scripts/makeOrderFromBasket.js
+++
@@ -1,8 +0,0 @@
-let makeOrder = async () => {
-    let btn = document.querySelector("#makeOrder");
-    btn.addEventListener("click", async () => {
-        console.log(123)
-        let totalPriceForAllItems = document.getElementById("totalPrice").textContent;
-        localStorage.setItem("price",totalPriceForAllItems);
-    })
-}
